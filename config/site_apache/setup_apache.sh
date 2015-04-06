3bbf56ec06a00d7907b57f49c79dbca5bd750a13
Anton Heuschen <antonfh@gmail.com>
2015-04-06 08:17:27 +0000

    Fix the path to the apache uafrica site setup

--- config/site_apache/setup_apache.sh
+++ config/site_apache/setup_apache.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install php5 php5-common php5-intl php5-cli php5-mcrypt php5-sqlite
-cp /var/www/carrier_rates/devtest01.uafrica.com.conf /etc/apache2/sites-available/devtest01.uafrica.com.conf
+cp /var/www/carrier_rates/config/site_apache/devtest01.uafrica.com.conf /etc/apache2/sites-available/devtest01.uafrica.com.conf
 a2ensite devtest01.uafrica.com.conf
 service apache2 restart
 a2enmod php5
