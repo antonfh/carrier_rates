59ab52d86ea72424384b71c1b78a9e4b3a8fcaac
Anton Heuschen <antonfh@gmail.com>
2015-04-06 17:41:56 +0000

    Build the Token request body

--- src/Controller/ShopifyAPIController.php
+++ src/Controller/ShopifyAPIController.php
@@ -75,9 +75,8 @@ class ShopifyAPIController extends AppController
             // Call our Shopify function
             $shopify_response = $this->shopify_call(NULL, $shop, "/admin/oauth/access_token", $query, 'POST');
             // Convert response into a nice and simple array
-            //$shopify_response = json_decode($shopify_response['response'], TRUE);
+            $shopify_response = json_decode($shopify_response['response'], TRUE);
             // Store the response
-            print_r($shopify_response);
             $token = $shopify_response['access_token'];
             // Show token (DO NOT DO THIS IN YOUR PRODUCTION ENVIRONMENT)
             echo $token;
